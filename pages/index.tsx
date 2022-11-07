a8e123ce8d150223eff9b3a3923e072bfcab3053
Suhel-Kap <suhelkapadia2@gmail.com>
2022-11-07 18:56:29 +0530

    all important stuff done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Title } from '@mantine/core';
 import Head from 'next/head'
 import {Layout} from "../components/Layout";
 
@@ -9,7 +10,7 @@ export default function Home() {
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
             </Head>
             <Layout>
-                Hello
+                <Title>The Crypto Studio</Title>
             </Layout>
         </>
     )

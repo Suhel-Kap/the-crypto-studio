41c764b5152f1e81e514edcb392a9d52b9076246
Suhel-Kap <suhelkapadia2@gmail.com>
2022-11-08 01:26:01 +0530

    favicon.ico updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -70,7 +70,7 @@ export default function App(props: AppProps) {
     return (
         <>
             <Head>
-                <title>Page title</title>
+                <title>The Crypto Studio</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
             </Head>
             <GlobalContext.Provider value={{user, setUser, group_id, channel_id, orbis} as any}>

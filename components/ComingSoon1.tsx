63430e1ee8bc7c0deb85a4c6de03ffb3ff0e982b
Suhel-Kap <suhelkapadia2@gmail.com>
2022-11-09 20:37:34 +0530

    its a wrap fin

---
+++ components/ComingSoon1.tsx
@@ -0,0 +1,55 @@
+import { createStyles, Paper, Text, ThemeIcon } from '@mantine/core';
+import {IconColorSwatch, IconPalette, TablerIcon} from '@tabler/icons';
+
+const useStyles = createStyles((theme) => ({
+    card: {
+        position: 'relative',
+        cursor: 'pointer',
+        overflow: 'hidden',
+        transition: 'transform 150ms ease, box-shadow 100ms ease',
+        padding: theme.spacing.xl,
+        paddingLeft: theme.spacing.xl * 2,
+
+        '&:hover': {
+            boxShadow: theme.shadows.md,
+            transform: 'scale(1.02)',
+        },
+
+        '&::before': {
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            width: 6,
+            backgroundImage: theme.fn.linearGradient(0, theme.colors.violet[3], theme.colors.indigo[9]),
+        },
+    },
+}));
+
+interface CardGradientProps {
+    title: string;
+    description: string;
+}
+
+export default function ComingSoon1({ title, description }: CardGradientProps) {
+    const { classes } = useStyles();
+    return (
+        <Paper withBorder radius="md" className={classes.card}>
+            <ThemeIcon
+                size="xl"
+                radius="md"
+                variant="gradient"
+                gradient={{ deg: 0, from: 'indigo', to: 'violet' }}
+            >
+                <IconPalette />
+            </ThemeIcon>
+            <Text size="xl" weight={500} mt="md">
+                {title}
+            </Text>
+            <Text size="sm" mt="sm" color="dimmed">
+                {description}
+            </Text>
+        </Paper>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
-import {Center, Container, createStyles, Grid, List, Stack, Text, ThemeIcon, Title} from '@mantine/core';
+import {Center, Container, createStyles, Grid, List, SimpleGrid, Stack, Text, ThemeIcon, Title} from '@mantine/core';
 import Head from 'next/head'
 import {Layout} from "../components/Layout";
 import {Hero} from "../components/Hero";
 import {Features} from "../components/Features";
-import {IconCheck, IconPencil} from "@tabler/icons";
+import {IconCheck} from "@tabler/icons";
 import {useEffect, useState} from "react";
 import {getTcsNfts} from "../utils/getTcsNfts";
 import NftCard from "../components/NftCard";
+import ComingSoon1 from "../components/ComingSoon1";
+import ComingSoon from "../components/ComingSoon";
 
 
 export default function Home() {
@@ -81,6 +83,23 @@ export default function Home() {
                             </List.Item>
                         </List>
                     </Stack>
+                </Center>
+                <Center mt={"xl"}>
+                    <Title>What's to come?</Title>
+                </Center>
+                <Center>
+                    <Container m={"xl"} mt={100} size={1000}>
+                        <Grid>
+                            <Grid.Col lg={6}>
+                                <ComingSoon title={"Documentation"}
+                                            description={"We also want to make it so that digital artists can come and build cool things by providing a guide of how to use our native Dynamic features written natively inside the CryptoStudio smart contract."}/>
+                            </Grid.Col>
+                            <Grid.Col lg={6}>
+                                <ComingSoon1 title={"Playground"}
+                                             description={"The future of CryptoStudio is to make it more accessible to the public. We want to make it so that anyone can create their own Dynamic NFTs with zero coding experience by creating a free and open to use web-based tool for designing and developing dynamic 2D & 3D NFT models. "}/>
+                            </Grid.Col>
+                        </Grid>
+                    </Container>
                 </Center>
                 <Container size={"xl"}>
                     <Center p={"xl"} m={"xl"}>
